79cb6b57e94ce6ad03b89ab5f93ec88eda6a7588
Ricardo Jaime Gil Simões <ricardo.gil.simoes@gmail.com>
2024-06-25 21:25:16 +0100

    fix: devAndroid Script

--- scripts/devAndroid.sh
+++ scripts/devAndroid.sh
@@ -1,6 +1,8 @@
- export ANDROID_HOME=/Users/ricar/Library/Android/sdk
- export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
- export NDK_HOME=/Users/ricar/Library/Android/sdk/ndk/27.0.11902837/
- export PATH="$PATH:$NDK_HOME"
+# Adjust paths for Android development
+export ANDROID_HOME=/Users/ricar/Library/Android/sdk
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
+export NDK_HOME=/Users/ricar/Library/Android/sdk/ndk/27.0.11902837/
+export PATH="$PATH:$NDK_HOME"
 
- pnpm tauri android dev Pixel_6_API_32
+# Adjust device Id
+pnpm tauri android dev Pixel_6_API_32
