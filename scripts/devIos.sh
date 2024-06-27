78af169875fec19044b7c0986709ef9f9a104472
Ricardo Jaime Gil Simões <ricardo.gil.simoes@gmail.com>
2024-06-27 20:38:28 +0100

    feat: iOS Build script

---
+++ scripts/devIos.sh
@@ -0,0 +1,5 @@
+# Adjust environment settings for iOS development
+export APPLE_DEVELOPMENT_TEAM=TH5KG5474M
+
+# Adjust device Id
+pnpm tauri ios dev 'Ryk Mobile'
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { IonReactRouter } from '@ionic/react-router';
-import TauriAPI from '@services/TauriAPI';
-import {
-  IonApp,
-  setupIonicReact
-} from '@ionic/react';
+import { IonApp, setupIonicReact } from '@ionic/react';
 import Tabs from '@components/tabs/Tabs';
 
 // Stylings
 import './global.scss';
 
-const ios = await TauriAPI.getPlatformName() === 'android' ? 'md' : 'ios';
 setupIonicReact({
-  mode: ios,
+  mode: 'ios',
   rippleEffect: true,
   animated: true,
-}
-);
+});
 
 const App: React.FC = () => (
   <IonApp>
